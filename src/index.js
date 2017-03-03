9fd8d0c0a3c375f38fd89ebed0494e7790dc7850
bennyva <benjaminvanarragon@gmail.com>
2017-03-03 14:32:52 +0000

    Added default app structure with create-react-app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//stores our main Render call from ReactDOM, imports app.js component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
